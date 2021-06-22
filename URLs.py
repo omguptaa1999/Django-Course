3e4aead3efff588b3558af15a1ebf37706560cc4
omguptaa1999 <53369276+omguptaa1999@users.noreply.github.com>
2021-06-22 16:06:57 +0530

    Create URLs.py

---
+++ URLs.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('', views.index, name='index')
+#]
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('', views.index, name='index'),
+#    path('removepunc', views.removepunc, name='rempun'),
+#    path('capitalizefirst', views.capfirst, name='capfirst'),
+#    path('newlineremove', views.newlineremove, name='newlineremove'),
+#    path('spaceremove', views.spaceremove, name='spaceremove'),
+#    path('charcount', views.charcount, name='charcount'),
+#]
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('analyze', views.analyze, name='analyze'),
+
+]
